8cb905a6e362851f8c018a8443b1fa2ac1e14688
Mayuresh Kadam <122379959+maykadam1@users.noreply.github.com>
2023-01-13 18:49:33 +0530

    Update streamlit_app.py
    error solved

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ except URLError as e:
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
- with my_cnx_cursor() as my_cur:
-  mycur.execute("select * from fruit_load_list")
+ with my_cnx.cursor() as my_cur:
+  my_cur.execute("select * from fruit_load_list")
   return my_cur.fetchall()
   
 if streamlit.button('Get Fruit Load List'):
