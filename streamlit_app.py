3e41aa4364578728324fd525d637520a05725c8e
Mayuresh Kadam <122379959+maykadam1@users.noreply.github.com>
2023-01-13 18:44:47 +0530

    Update streamlit_app.py
    Remove streamlit stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
  with my_cnx_cursor() as my_cur:
